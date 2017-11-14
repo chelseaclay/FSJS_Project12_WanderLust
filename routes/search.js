5cef4dce7d4dea7ba8d56168d001550dc929c206
chelseaclay <chelseaclayyy@yahoo.com>
2017-11-14 09:27:24 -0500

    Related content on details page leads to details of selected related content. Error when search fireld is empty.

--- routes/search.js
+++ routes/search.js
@@ -17,52 +17,55 @@ var relatedContent;
 router.post('/', function(req, res, next) {
   bookself = [];
 
-  if(req.body.filter === 'books') {
-    mediaType = 'books';
-    // Google Books API
-    books.search(req.body.search, {
-      key: config.googleKey,
-      type: 'books'
-    }, function(error, results) {
-        var googlePromise = new Promise((resolve, reject) => {
-          resolve(bookself.push(results));
-        });
-
-        googlePromise.then(response => {
-          res.render('gallery', {
-             title: 'Express',
-             items: bookself[0],
-             book: true
-           });
-
-        }).catch(error => {
-          console.log(error);
-        });
-    });
-  } else if(req.body.filter === 'movies') {
-    mediaType = 'movies';
-    // iTunes API
-    axios.get('https://itunes.apple.com/search?', {
-      params: {
-        term: req.body.search,
-        media: 'movie'
-      }
-
-    }).then(response => {
-        bookself.push(response.data);
-
-    }).then(response => {
-      res.render('gallery', {
-         title: 'Express',
-         items: bookself[0].results,
-         book: false
-       });
-
-    }).catch(error => {
-      console.log(error);
-    });
+  if(!req.body.search){
+      // need to catch when search is empty
+      console.log(error);
   } else {
-    // need to catch when search is empty
+    if(req.body.filter === 'books') {
+      mediaType = 'books';
+      // Google Books API
+      books.search(req.body.search, {
+        key: config.googleKey,
+        type: 'books'
+      }, function(error, results) {
+          var googlePromise = new Promise((resolve, reject) => {
+            resolve(bookself.push(results));
+          });
+
+          googlePromise.then(response => {
+            res.render('gallery', {
+               title: 'Express',
+               items: bookself[0],
+               book: true
+             });
+
+          }).catch(error => {
+            console.log(error);
+          });
+      });
+    } else if(req.body.filter === 'movies') {
+      mediaType = 'movies';
+      // iTunes API
+      axios.get('https://itunes.apple.com/search?', {
+        params: {
+          term: req.body.search,
+          media: 'movie'
+        }
+
+      }).then(response => {
+          bookself.push(response.data);
+
+      }).then(response => {
+        res.render('gallery', {
+           title: 'Express',
+           items: bookself[0].results,
+           book: false
+         });
+
+      }).catch(error => {
+        console.log(error);
+      });
+    }
   }
 
 
@@ -103,6 +106,7 @@ router.get('/:id', function(req, res, next) {
           itemImg: details[0].thumbnail,
           itemId: details[0].id,
           related: relatedContent,
+          contentType: 'Book',
           added: false
         });
     }).catch(error => {
@@ -140,6 +144,7 @@ router.get('/:id', function(req, res, next) {
           itemRating: details[0].contentAdvisoryRating,
           itemId: details[0].trackId,
           related: relatedContent,
+          contentType: 'Movie',
           added: false
         });
     }).catch(error => {
@@ -150,7 +155,7 @@ router.get('/:id', function(req, res, next) {
 });
 
 /////////////////////////////////////////
-/* GET searched item details to Library. */
+/* Add content to user's to Library. */
 /////////////////////////////////////////
 router.get('/:id/add', mid.requiresLogin, function(req, res, next) {
   if(mediaType === 'books') {
@@ -266,6 +271,113 @@ router.get('/:id/add', mid.requiresLogin, function(req, res, next) {
       }
     });
   }
+});
+
+/////////////////////////////////////////
+/* GET related Book searched. */
+/////////////////////////////////////////
+router.get('/:id/relatedBook', function(req, res, next) {
+  bookself = [];
+
+  // Google Books API
+  books.search(req.params.id, {
+    key: config.googleKey,
+    type: 'books'
+  }, function(error, results) {
+      var googlePromise = new Promise((resolve, reject) => {
+        resolve(bookself.push(results));
+      });
+
+       details = bookself[0];
+
+       // TasteDive API
+       axios.get('https://tastedive.com/api/similar?', {
+         params: {
+           q: details[0].title,
+           k: config.tasteDiveKey,
+           limit: 5,
+           type: 'books'
+         }
+
+       }).then(response => {
+           relatedContent = response.data.Similar.Results;
+
+       }).then(response => {
+           res.render('details', {
+             title: 'Express',
+             itemTitle: details[0].title,
+             itemAuthor: details[0].authors,
+             itemGenre: details[0].categories,
+             itemYear: details[0].publishedDate,
+             itemDescription: details[0].description,
+             itemImg: details[0].thumbnail,
+             itemId: details[0].id,
+             related: relatedContent,
+             contentType: 'Book',
+             added: false
+           });
+       }).catch(error => {
+         console.log(error);
+       });
+  });
+
+});
+
+
+/////////////////////////////////////////
+/* GET related Movie searched. */
+/////////////////////////////////////////
+router.get('/:id/relatedMovie', function(req, res, next) {
+  bookself = [];
+  // iTunes API
+  axios.get('https://itunes.apple.com/search?', {
+    params: {
+      term: req.params.id,
+      media: 'movie'
+    }
+
+  }).then(response => {
+      bookself.push(response.data);
+
+  }).then(response => {
+
+     details = bookself[0].results
+
+     // TasteDive API
+     axios.get('https://tastedive.com/api/similar?', {
+       params: {
+         q: details[0].trackName,
+         k: config.tasteDiveKey,
+         limit: 5,
+         type: 'movies'
+       }
+
+     }).then(response => {
+         relatedContent = response.data.Similar.Results;
+
+     }).then(response => {
+         res.render('details', {
+           title: 'Express',
+           itemTitle: details[0].trackName,
+           itemAuthor: details[0].artistName,
+           itemGenre: details[0].primaryGenreName,
+           itemYear: details[0].releaseDate,
+           itemDescription: details[0].longDescription,
+           itemImg: details[0].artworkUrl100,
+           itemRating: details[0].contentAdvisoryRating,
+           itemId: details[0].trackId,
+           related: relatedContent,
+           contentType: 'Movie',
+           added: false
+         });
+     }).catch(error => {
+       console.log(error);
+     });
+
+  }).catch(error => {
+    console.log(error);
+  });
+
 });
 
 module.exports = router;
